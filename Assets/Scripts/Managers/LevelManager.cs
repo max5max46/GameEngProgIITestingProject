9549c9d5b1179c922e925ff2f0663e32f3d97a12
max5max46 <113365542+max5max46@users.noreply.github.com>
2024-09-27 15:29:19 -0300

    Added Loading Screens

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor.SearchService;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -17,7 +18,7 @@ public class LevelManager : MonoBehaviour
 
 
     public int nextScene;
-
+    public List<AsyncOperation> scenesToLoad = new List<AsyncOperation>();
 
     public void Awake()
     {
@@ -38,7 +39,7 @@ public class LevelManager : MonoBehaviour
     {
         nextScene = SceneManager.GetActiveScene().buildIndex + 1;
         LoadScene(nextScene);
-        _gameStateManager.SwitchToState(_gameStateManager.gameState_GamePlay);       
+        _gameStateManager.SwitchToState(_gameStateManager.gameState_GamePlay);
     }
 
 
@@ -59,7 +60,55 @@ public class LevelManager : MonoBehaviour
         Application.Quit();
     }
 
+    public void LoadScene(string sceneName)
+    {
+        switch (sceneName)
+        {
+            case "MainMenu":
+                _uIManager.UILoadingScreen(_uIManager.mainMenuUI);
+                _gameStateManager.SwitchToState(_gameStateManager.gameState_GameInit);
+                break;
 
+            case "TestLevel":
+                _uIManager.UILoadingScreen(_uIManager.gamePlayUI);
+                _gameStateManager.SwitchToState(_gameStateManager.gameState_GamePlay);
+                break;
+
+            default:
+                sceneName = "MainMenu";
+                _uIManager.UILoadingScreen(_uIManager.mainMenuUI);
+                break;
+        }
+
+        StartCoroutine(WaitForScreenLoad(sceneName));
+    }
+
+    private IEnumerator WaitForScreenLoad(string sceneName)
+    {
+        yield return new WaitForSeconds(_uIManager.fadeTime);
+        Debug.Log("Loading Scene Starting");
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.completed += OperationCompleted;
+        scenesToLoad.Add(operation);
+    }
+
+    public float GetLoadingProgress()
+    {
+        float totalprogress = 0;
+
+        foreach (AsyncOperation operation in scenesToLoad)
+        {
+            totalprogress += operation.progress;
+        }
+
+        return totalprogress / scenesToLoad.Count;
+    }
+
+    private void OperationCompleted(AsyncOperation operation)
+    {
+        scenesToLoad.Remove(operation);
+    }
 
 }
 
--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,26 +1,36 @@
 ﻿using System.Collections;
 using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
 // Sam Robichaud 
 // NSCC Truro 2024
 // This work is licensed under CC BY-NC-SA 4.0 (https://creativecommons.org/licenses/by-nc-sa/4.0/)
 
 public class UIManager : MonoBehaviour
-{    
-
+{
+    [Header("Object Connections")]
+    public LevelManager levelManager;
 
     // References to UI Panels
+    [Header("UI Screens")]
     public GameObject mainMenuUI;
     public GameObject gamePlayUI;
     public GameObject gameOverUI;
     public GameObject pauseMenuUI;
     public GameObject optionsMenuUI;
     public GameObject creditsMenuUI;
+    public GameObject LoadingScreen;
 
-
+    [Header("Gameplay UI Elements")]
     // Gameplay Specific UI Elements
     public Text LevelCount;
+
+    [Header("Loading Screen UI Elements")]
+    public CanvasGroup loadingScreenCanvasGroup;
+    public Image loadingBar;
+    public TextMeshProUGUI loadingText;
+    public float fadeTime = 0.5f;
 
     public void UpdateLevelCount(int count)
     {
@@ -69,6 +79,11 @@ public class UIManager : MonoBehaviour
         creditsMenuUI.SetActive(true);
     }
 
+    public void UILoadingScreen(GameObject targetPanel)
+    {
+        StartCoroutine(LoadingUIFadeIN());
+        StartCoroutine(DelayedSwitchUIPanel(fadeTime, targetPanel));
+    }
 
     public void DisableAllUIPanels()
     {
@@ -87,9 +102,70 @@ public class UIManager : MonoBehaviour
         gameOverUI.SetActive(true);
         pauseMenuUI.SetActive(true);
         optionsMenuUI.SetActive(true);
-        creditsMenuUI.SetActive(false);
+        creditsMenuUI.SetActive(true);
     }
 
+    private IEnumerator LoadingUIFadeOut()
+    {
+        Debug.Log("Starting Fadeout");
 
+        float timer = 0;
 
+        while (timer < fadeTime)
+        {
+            loadingScreenCanvasGroup.alpha = Mathf.Lerp(1, 0, timer / fadeTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        loadingScreenCanvasGroup.alpha = 0;
+        LoadingScreen.SetActive(false);
+        loadingBar.fillAmount = 0;
+
+        Debug.Log("Ending Fadeout");
+    }
+
+    private IEnumerator LoadingUIFadeIN()
+    {
+        Debug.Log("Starting Fadein");
+        float timer = 0;
+        LoadingScreen.SetActive(true);
+
+        while (timer < fadeTime)
+        {
+            loadingScreenCanvasGroup.alpha = Mathf.Lerp(0, 1, timer / fadeTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        loadingScreenCanvasGroup.alpha = 1;
+
+        Debug.Log("Ending Fadein");
+        StartCoroutine(LoadingBarProgress());
+    }
+
+    private IEnumerator LoadingBarProgress()
+    {
+        Debug.Log("Starting Progress Bar");
+        while (levelManager.scenesToLoad.Count <= 0)
+        {
+            //waiting for loading to begin
+            yield return null;
+        }
+        while (levelManager.scenesToLoad.Count > 0)
+        {
+            loadingBar.fillAmount = levelManager.GetLoadingProgress();
+            yield return null;
+        }
+        yield return new WaitForEndOfFrame();
+        Debug.Log("Ending Progress Bar");
+        StartCoroutine(LoadingUIFadeOut());
+    }
+
+    private IEnumerator DelayedSwitchUIPanel(float time, GameObject uiPanel)
+    {
+        yield return new WaitForSeconds(time);
+        DisableAllUIPanels();
+        uiPanel.SetActive(true);
+    }
 }
