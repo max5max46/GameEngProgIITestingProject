c86a42db3d0f91c1e4483d5cddabf3720d8890d0
Dylan Adams <dylan.adams.2016@outlook.com>
2024-10-16 15:28:15 -0300

    Added Raycasts and a color changing cube

---
+++ Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private LayerMask cubeLayerMask;
+    [SerializeField] private LayerMask ground;
+
+    private void Awake()
+    {
+        playerCamera = Camera.main;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = 10;
+
+        Debug.DrawLine(playerCamera.transform.position, playerCamera.transform.forward * 20, Color.red);
+
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out RaycastHit hit, 20, ground.value))
+        {
+            distance = hit.distance;
+        }
+
+        RaycastHit[] hits = Physics.RaycastAll(playerCamera.transform.position, playerCamera.transform.forward, distance, cubeLayerMask.value);
+
+        foreach (RaycastHit hitSingle in hits)
+        {
+            if (hitSingle.collider.GetComponent<Renderer>())
+            {
+                hitSingle.collider.GetComponent<Renderer>().material.color = Color.blue;
+                Debug.Log("Name: " + hitSingle.collider.gameObject.name + " | Distance: " + hitSingle.distance);
+            }
+        }
+
+        //if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out RaycastHit hit, 20, cubeLayerMask.value))
+        //{
+        //    if (hit.collider.GetComponent<Renderer>())
+        //    {
+        //        Debug.Log("Hit");
+        //        hit.collider.GetComponent<Renderer>().material.color = Color.blue;
+        //    }
+        //}
+    }
+}
